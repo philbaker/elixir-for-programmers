f83829d07b28a348cd3637d3571e64386c9d9391
philbaker <phil@philbaker.me>
2021-12-08 06:35:24 +0000

    Add pipelines

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,31 +1,23 @@
 defmodule Dictionary do
-  def word_list do
-    words = File.read!("assets/words.txt")
-
-    String.split(words, ~r/\n/, trim: true)
-  end
+  @word_list "assets/words.txt"
+  |> File.read!()
+  |> String.split(~r/\n/, trim: true)
 
   def random_word do
-    Enum.random(word_list())
+    @word_list
+    |> Enum.random()
   end
 
   def hello do
-    IO.puts "Hello world!!"
+    "hiya"
   end
 end
 
-# Dictionary.hello
+Dictionary.hello
 
-# r Dictionary
-
-# Dictionary.hello
-
-# Dictionary.word_list
-
-# Dictionary.random_word
+Dictionary.random_word
 
 # memorable_quote = "had we but world enough, and time"
-
 # String.split(memorable_quote, ",", trim: true)
 
 # letters = String.split(memorable_quote, "")
@@ -39,3 +31,51 @@ end
 # String.reverse(memorable_quote)
 
 # String.myers_difference(memorable_quote, "had we but bacon enough, and treacle")
+
+# Enum.join(Enum.sort(String.split("hello", "")))
+
+# "hello"
+#   |> String.split("")
+#   |> Enum.sort
+#   |> Enum.join
+
+
+# "assets/words.txt"
+#   |> File.read!
+#   |> String.split(~r/\n/)
+#   |> Kernel.length
+
+# "hello\tworld\ttabs\trule"
+  # |> String.split(~r/\t/)
+#   |> Enum.join(",")
+#
+# Numbers
+
+# 8 / 3
+
+# div(8, 3)
+
+# trunc(8/3)
+
+# round(8/3)
+
+# String.to_integer("65");
+
+String.to_integer("41", 16)
+
+
+# String.to_integer("1000001", 2)
+
+# Atoms
+# Constants that are used to name things
+# a = 99
+# => 99
+
+# :"next-number: #{a+1}"
+# => :"next-number: 100"
+
+# is_atom(Dictionary)
+# => true
+
+# fname = "/etc/passwd"
+# => "/etc/passwd"
