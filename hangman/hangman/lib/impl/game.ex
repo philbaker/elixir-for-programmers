9050e26e46274b30c9782369ef5270cb1a467648
philbaker <phil@philbaker.me>
2021-12-19 20:21:03 +0000

    Score a good guess

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -1,8 +1,10 @@
 defmodule Hangman.Impl.Game do
 
-  @type t :: %Hangman.Impl.Game{
+  alias Hangman.Type
+
+  @type t :: %__MODULE__{
     turns_left: integer,
-    game_state: Hangman.state,
+    game_state: Type.state,
     letters: list(String.t),
     used: MapSet.t(String.t)
   }
@@ -14,11 +16,64 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
+  @spec new_game() :: t
   def new_game do
+    new_game(Dictionary.random_word)
+  end
+
+  @spec new_game(String.t) :: t
+  def new_game(word) do
     %__MODULE__{
-      letters: Dictionary.random_word |> String.codepoints
+        letters: word |> String.codepoints
+      }
+  end
+
+  @spec make_move(t, String.t) :: { t, Type.tally }
+  def make_move(game = %{ game_state: state }, _guess)
+  when state in [:won, :lost] do
+    game
+    |> return_with_tally()
+  end
+
+  def make_move(game, guess) do
+    accept_guess(game, guess, MapSet.member?(game.used, guess))
+    |> return_with_tally()
+  end
+
+  defp accept_guess(game, _guess, _already_used = true) do
+    %{ game | game_state: :already_used }
+  end
+
+  defp accept_guess(game, guess, _already_used) do
+    %{ game | used: MapSet.put(game.used, guess) }
+    |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  defp score_guess(game, _good_guess = true) do
+    new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{ game | game_state: new_state }
+  end
+
+  defp score_guess(game, _bad_guess) do
+    game
+    # turns_left == 1 -> lost | dec turns_left, :bad_guess
+  end
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list |> Enum.sort,
     }
   end
+
+  defp return_with_tally(game) do
+    { game, tally(game) }
+  end
+
+  defp maybe_won(true), do: :won
+  defp maybe_won(_), do: :good_guess
 end
 
 # Hangman.Impl.Game.new_game
---
+++ hangman/hangman/test/impl_game_test.exs
@@ -0,0 +1,47 @@
+defmodule ImplGameTest do
+  use ExUnit.Case
+  alias Hangman.Impl.Game
+
+  test "new game returns structure" do
+    game = Game.new_game
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "new game returns correct word" do
+    game = Game.new_game("wombat")
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+    assert Enum.any?('wombat', fn x -> (x <= 32) || (x >= 255) end) == false
+  end
+
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, state)
+      { new_game, _tally } = Game.make_move(game, "x")
+      assert new_game == game
+    end
+  end
+
+  test "a duplicate letter is reported" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "we record letters used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    { game, _tally } = Game.make_move(game, "y")
+    { game, _tally } = Game.make_move(game, "x")
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
+  end
+end
