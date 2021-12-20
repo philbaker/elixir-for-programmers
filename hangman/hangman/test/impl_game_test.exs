ba5d91bf87d009d4872d5d35a0cf7687f13c80bd
philbaker <phil@philbaker.me>
2021-12-20 20:38:21 +0000

    Add game tests

--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -44,4 +44,30 @@ defmodule ImplGameTest do
     { game, _tally } = Game.make_move(game, "x")
     assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
   end
+
+  test "can handle a sequence of move" do
+    [
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["e", :good_guess, 6, ["_", "e", "_", "_", "_"], ["a", "e"]],
+      ["x", :bad_guess, 6, ["_", "e", "_", "_", "_"], ["a", "e", "x"]]
+    ]
+    |> test_sequence_of_moves()
+  end
+
+  def test_sequence_of_moves(script) do
+    game = Game.new_game("hello")
+    Enum.reduce(script, game, &check_one_move/2)
+  end
+
+  defp check_one_move([ guess, state, turns, letters, used ], game) do
+    { game, tally } = Game.make_move(game, guess)
+
+    assert tally.game_state == state
+    assert tally.turns_left == turns
+    assert tally.letters    == letters
+    assert tally.used       == used
+
+    game
+  end
 end
