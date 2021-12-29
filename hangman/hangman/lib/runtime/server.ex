4df9b2c95ab26a8257804c3b68e8cf00da039bfc
philbaker <phil@philbaker.me>
2021-12-29 14:33:45 +0000

    Experiment with nodes

--- hangman/hangman/lib/runtime/server.ex
+++ hangman/hangman/lib/runtime/server.ex
@@ -1,5 +1,6 @@
 defmodule Hangman.Runtime.Server do
 
+  @type t :: pid
   alias Hangman.Impl.Game
   use GenServer
 
@@ -11,5 +12,14 @@ defmodule Hangman.Runtime.Server do
   # Server process
   def init(_) do
     { :ok, Game.new_game }
+  end
+
+  def handle_call({ :make_move, guess }, _from, game) do
+    { updated_game, tally } = Game.make_move(game, guess)
+    { :reply, tally, updated_game }
+  end
+
+  def handle_call({ :tally }, _from, game) do
+    { :reply, Game.tally(game), game }
   end
 end
